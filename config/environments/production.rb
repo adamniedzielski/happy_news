a2ba8688ad7835901c6a8d398ce93afa84e29f8b
Adam Niedzielski <adamsunday@gmail.com>
2020-11-20 07:08:44 +0100

    Send emails with Gmail (#2)
    * Send emails with Gmail
    
    * Set secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,4 +97,14 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: ENV.fetch("GMAIL_USERNAME"),
+    password: ENV.fetch("GMAIL_PASSWORD"),
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 end
