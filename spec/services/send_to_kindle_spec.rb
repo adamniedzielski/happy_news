ebd734ed6a672ad374401b3536e46001b0aea80f
Adam Niedzielski <adamsunday@gmail.com>
2021-01-08 16:49:54 +0100

    Convert content to ISO 8859-1 (#10)

--- spec/services/send_to_kindle_spec.rb
+++ spec/services/send_to_kindle_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe SendToKindle do
     expect(ActionMailer::Base.deliveries.size).to eq 1
     expect(ActionMailer::Base.deliveries.last.to).to eq ["test@kindle.com"]
   end
+
+  it "sends the attachment in ISO-8859-1 encoding" do
+    RSpec::Matchers.define :iso_8859_1_encoded do
+      match { |actual| actual.encoding == Encoding::ISO_8859_1 }
+    end
+
+    service = SendToKindle.new(receiver: "test@kindle.com")
+
+    expect(KindleMailer).to receive(:call)
+      .with(anything, iso_8859_1_encoded)
+      .and_call_original
+
+    service.call("Üäößß")
+  end
 end
