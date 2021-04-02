20a8a18f0f06c6d9f2d0a0e6d3e644725249167f
Adam Niedzielski <adamsunday@gmail.com>
2021-04-02 16:09:10 +0200

    Get Berliner Zeitung article content (#24)

---
+++ app/services/get_berliner_zeitung_article_content.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class GetBerlinerZeitungArticleContent
+  def initialize(http_client: HTTParty)
+    self.http_client = http_client
+  end
+
+  def call(url)
+    page = Nokogiri::HTML(http_client.get(url).body)
+
+    title = page.at(".a-storyhead")
+    leading_content = page.at(".a-storylead")
+    content = page.at(".main-content")
+
+    "#{title} #{leading_content} #{content.inner_html}"
+  end
+
+  private
+
+  attr_accessor :http_client
+end
---
+++ spec/services/get_berliner_zeitung_article_content_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "test_helpers/mock_http_client"
+
+RSpec.describe GetBerlinerZeitungArticleContent do
+  it "scrapes article title" do
+    http_client = MockHTTPClient.new("berliner-zeitung-article.html")
+    service = GetBerlinerZeitungArticleContent.new(http_client: http_client)
+    result = service.call(
+      "https://www.berliner-zeitung.de/mensch-metropole/berliner-forscher-bitten-zu-ostern-zur-hasenjagd-li.150204"
+    )
+
+    expect(result).to include(
+      "Berliner Forscher bitten zu Ostern zur Hasenjagd"
+    )
+  end
+
+  it "scrapes the leading content" do
+    http_client = MockHTTPClient.new("berliner-zeitung-article.html")
+    service = GetBerlinerZeitungArticleContent.new(http_client: http_client)
+    result = service.call(
+      "https://www.berliner-zeitung.de/mensch-metropole/berliner-forscher-bitten-zu-ostern-zur-hasenjagd-li.150204"
+    )
+
+    expect(result).to include(
+      "Vor allem im Ostteil ist immer öfter der echte Hase zu sehen."
+    )
+  end
+
+  it "scrapes the rest of the article" do
+    http_client = MockHTTPClient.new("berliner-zeitung-article.html")
+    service = GetBerlinerZeitungArticleContent.new(http_client: http_client)
+    result = service.call(
+      "https://www.berliner-zeitung.de/mensch-metropole/berliner-forscher-bitten-zu-ostern-zur-hasenjagd-li.150204"
+    )
+
+    expect(result).to include(
+      "Hasen sind lernfähig und sehen Menschen in der Stadt nicht als Feinde an"
+    )
+  end
+end
