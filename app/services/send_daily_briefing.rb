56aa55339f234c3890903462a20da131e26a0899
Adam Niedzielski <adamsunday@gmail.com>
2021-04-03 18:16:43 +0200

    Include Berliner Zeitung in briefing

--- app/services/send_daily_briefing.rb
+++ app/services/send_daily_briefing.rb
@@ -4,20 +4,28 @@ class SendDailyBriefing
   def initialize(
     banned_phrases:, get_rbb_articles: GetRbbArticles.new,
     get_rbb_article_content: GetRbbArticleContent.new,
+    get_berliner_zeitung_articles: GetBerlinerZeitungArticles.new,
+    get_berliner_zeitung_article_content: GetBerlinerZeitungArticleContent.new,
     send_to_kindle: SendToKindle.new
   )
+    self.banned_phrases = banned_phrases
     self.get_rbb_articles = get_rbb_articles
     self.get_rbb_article_content = get_rbb_article_content
+    self.get_berliner_zeitung_articles = get_berliner_zeitung_articles
+    self.get_berliner_zeitung_article_content = get_berliner_zeitung_article_content
     self.send_to_kindle = send_to_kindle
-    self.banned_phrases = banned_phrases
   end
 
   def call
-    articles = get_rbb_articles.call
+    articles = get_rbb_articles.call + get_berliner_zeitung_articles.call
     happy_articles = reject_unhappy(articles)
 
     body = happy_articles.map do |article|
-      get_rbb_article_content.call(article.link)
+      if article.link.start_with?("https://www.rbb24.de")
+        get_rbb_article_content.call(article.link)
+      else
+        get_berliner_zeitung_article_content.call(article.link)
+      end
     end.join(" ")
 
     document_name = "Happy Briefing #{Time.current.strftime('%d.%m')}"
@@ -37,7 +45,11 @@ class SendDailyBriefing
 
   private
 
-  attr_accessor :get_rbb_articles, :get_rbb_article_content, :send_to_kindle, :banned_phrases
+  attr_accessor(
+    :banned_phrases, :get_rbb_articles, :get_rbb_article_content,
+    :get_berliner_zeitung_articles, :get_berliner_zeitung_article_content,
+    :send_to_kindle
+  )
 
   def reject_unhappy(articles)
     articles.reject do |article|
