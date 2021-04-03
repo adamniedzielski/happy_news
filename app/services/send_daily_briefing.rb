5bf4e2247f7c7f0d678f8c498db8dadd85579847
Adam Niedzielski <adamsunday@gmail.com>
2021-04-03 18:29:59 +0200

    Refactor to address Rubocop offences

--- app/services/send_daily_briefing.rb
+++ app/services/send_daily_briefing.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class SendDailyBriefing
+  # rubocop:disable Metrics/ParameterLists
   def initialize(
     banned_phrases:, get_rbb_articles: GetRbbArticles.new,
     get_rbb_article_content: GetRbbArticleContent.new,
@@ -15,32 +16,18 @@ class SendDailyBriefing
     self.get_berliner_zeitung_article_content = get_berliner_zeitung_article_content
     self.send_to_kindle = send_to_kindle
   end
+  # rubocop:enable Metrics/ParameterLists
 
   def call
     articles = get_rbb_articles.call + get_berliner_zeitung_articles.call
     happy_articles = reject_unhappy(articles)
 
     body = happy_articles.map do |article|
-      if article.link.start_with?("https://www.rbb24.de")
-        get_rbb_article_content.call(article.link)
-      else
-        get_berliner_zeitung_article_content.call(article.link)
-      end
+      get_content(article)
     end.join(" ")
 
     document_name = "Happy Briefing #{Time.current.strftime('%d.%m')}"
-    document =
-      <<~HEREDOC
-        <!DOCTYPE html>
-        <html lang="en">
-          <head></head>
-          <body>
-            #{body}
-          </body>
-        </html>
-      HEREDOC
-
-    send_to_kindle.call(document_name, document)
+    send_to_kindle.call(document_name, format_document(body))
   end
 
   private
@@ -57,5 +44,25 @@ class SendDailyBriefing
         "#{article.title} #{article.description}".downcase.include?(phrase)
       end
     end
+  end
+
+  def get_content(article)
+    if article.link.start_with?("https://www.rbb24.de")
+      get_rbb_article_content.call(article.link)
+    else
+      get_berliner_zeitung_article_content.call(article.link)
+    end
+  end
+
+  def format_document(body)
+    <<~HEREDOC
+      <!DOCTYPE html>
+      <html lang="en">
+        <head></head>
+        <body>
+          #{body}
+        </body>
+      </html>
+    HEREDOC
   end
 end
